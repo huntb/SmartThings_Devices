0a5955df016f53887f2126c980daffce52c74b72
huntb <bchunt3@gmail.com>
2014-02-07 11:32:46 -0500

    Added BetterDimmer Device Type

---
+++ BetterDimmer.device.groovy
@@ -0,0 +1,260 @@
+/*****************************************************************************
+Device:  BetterDimmer.device.groovy
+Author:  twack@wackware.net modified by Ben Hunt
+Version: 0.1
+Date:    2014-2-6
+Purpose: To implement horizontal slider and up/down toggle in physical dimmer
+         device. This is a stand alone device used with Z-Wave dimmers
+
+Use License: Non-Profit Open Software License version 3.0 (NPOSL-3.0)
+             http://opensource.org/licenses/NPOSL-3.0
+
+******************************************************************************
+                                Change Log
+
+Version:  0.1
+Date:     20140207
+Change1:  Initial Release
+
+******************************************************************************
+
+Device Type:	Switch, Switch Level, Refresh, Polling
+
+Custom
+Commands:		levelUp
+				levelDown
+                updateLevel
+                
+
+Custom
+Attributes:		None
+
+                
+Other Info:		Special thanks to @twack for helping with the the source code. 
+				Much of the other code has been taken from the Dimmer Switch 
+                device type.
+
+******************************************************************************/
+metadata {
+	simulator {
+		status "on":  "command: 2003, payload: FF"
+		status "off": "command: 2003, payload: 00"
+		status "09%": "command: 2003, payload: 09"
+		status "10%": "command: 2003, payload: 0A"
+		status "33%": "command: 2003, payload: 21"
+		status "66%": "command: 2003, payload: 42"
+		status "99%": "command: 2003, payload: 63"
+
+		// reply messages
+		reply "2001FF,delay 5000,2602": "command: 2603, payload: FF"
+		reply "200100,delay 5000,2602": "command: 2603, payload: 00"
+		reply "200119,delay 5000,2602": "command: 2603, payload: 19"
+		reply "200132,delay 5000,2602": "command: 2603, payload: 32"
+		reply "20014B,delay 5000,2602": "command: 2603, payload: 4B"
+		reply "200163,delay 5000,2602": "command: 2603, payload: 63"
+	}
+
+	tiles {
+		standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: true) {
+			state "on", label:'${name}', action:"switch.off", icon:"st.switches.switch.on", backgroundColor:"#79b821", nextState:"turningOff"
+			state "off", label:'${name}', action:"switch.on", icon:"st.switches.switch.off", backgroundColor:"#ffffff", nextState:"turningOn"
+			state "turningOn", label:'${name}', icon:"st.switches.switch.on", backgroundColor:"#79b821"
+			state "turningOff", label:'${name}', icon:"st.switches.switch.off", backgroundColor:"#ffffff"
+		}
+		standardTile("lUp", "device.switch", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
+                        state "up", label:'', action:"levelUp",icon:"st.illuminance.illuminance.bright"
+        }
+        standardTile("lDown", "device.switch", inactiveLabel: false,decoration: "flat", canChangeIcon: false) {
+                        state "down", label:'', action:"levelDown",icon:"st.illuminance.illuminance.light"
+        }
+		standardTile("indicator", "device.indicatorStatus", inactiveLabel: false, decoration: "flat") {
+			state "when off", action:"indicator.indicatorWhenOn", icon:"st.indicators.lit-when-off"
+			state "when on", action:"indicator.indicatorNever", icon:"st.indicators.lit-when-on"
+			state "never", action:"indicator.indicatorWhenOff", icon:"st.indicators.never-lit"
+		}
+		
+		standardTile("refresh", "device.switch", inactiveLabel: false, decoration: "flat") {
+			state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+		}
+		controlTile("levelSliderControl", "device.level", "slider", height: 1, width: 2, inactiveLabel: false) {
+			state "level", action:"switch level.setLevel"
+		}		
+		valueTile("lValue", "device.level", inactiveLabel: true, height:1, width:1, decoration: "flat") {
+            state "levelValue", label:'${currentValue}%', unit:"", backgroundColor: "#53a7c0"
+        }
+		
+
+		main(["switch"])
+		details(["switch", "lUp", "lDown", "levelSliderControl", "lValue", "indicator", "refresh"])
+	}
+}
+
+def parse(String description) {
+	def item1 = [
+		canBeCurrentState: false,
+		linkText: getLinkText(device),
+		isStateChange: false,
+		displayed: false,
+		descriptionText: description,
+		value:  description
+	]
+	def result
+	def cmd = zwave.parse(description, [0x20: 1, 0x26: 1, 0x70: 1])
+	if (cmd) {
+		result = createEvent(cmd, item1)
+	}
+	else {
+		item1.displayed = displayed(description, item1.isStateChange)
+		result = [item1]
+	}
+	log.debug "Parse returned ${result?.descriptionText}"
+	result
+}
+
+def createEvent(physicalgraph.zwave.commands.basicv1.BasicReport cmd, Map item1) {
+	def result = doCreateEvent(cmd, item1)
+	for (int i = 0; i < result.size(); i++) {
+		result[i].type = "physical"
+	}
+	result
+}
+
+def createEvent(physicalgraph.zwave.commands.switchmultilevelv1.SwitchMultilevelReport cmd, Map item1) {
+	def result = doCreateEvent(cmd, item1)
+	result[0].descriptionText = "${item1.linkText} is ${item1.value}"
+	result[0].handlerName = cmd.value ? "statusOn" : "statusOff"
+	for (int i = 0; i < result.size(); i++) {
+		result[i].type = "digital"
+	}
+	result
+}
+
+def doCreateEvent(physicalgraph.zwave.Command cmd, Map item1) {
+	def result = [item1]
+
+	item1.name = "switch"
+	item1.value = cmd.value ? "on" : "off"
+	item1.handlerName = item1.value
+	item1.descriptionText = "${item1.linkText} was turned ${item1.value}"
+	item1.canBeCurrentState = true
+	item1.isStateChange = isStateChange(device, item1.name, item1.value)
+	item1.displayed = item1.isStateChange
+
+	if (cmd.value >= 5) {
+		def item2 = new LinkedHashMap(item1)
+		item2.name = "level"
+		item2.value = cmd.value as String
+		item2.unit = "%"
+		item2.descriptionText = "${item1.linkText} dimmed ${item2.value} %"
+		item2.canBeCurrentState = true
+		item2.isStateChange = isStateChange(device, item2.name, item2.value)
+		item2.displayed = false
+		result << item2
+	}
+	result
+}
+
+def zwaveEvent(physicalgraph.zwave.commands.configurationv1.ConfigurationReport cmd) {
+	def value = "when off"
+	if (cmd.configurationValue[0] == 1) {value = "when on"}
+	if (cmd.configurationValue[0] == 2) {value = "never"}
+	[name: "indicatorStatus", value: value, display: false]
+}
+
+def createEvent(physicalgraph.zwave.Command cmd,  Map map) {
+	// Handles any Z-Wave commands we aren't interested in
+	log.debug "UNHANDLED COMMAND $cmd"
+}
+
+def on() {
+	log.info "on"
+	delayBetween([zwave.basicV1.basicSet(value: 0xFF).format(), zwave.switchMultilevelV1.switchMultilevelGet().format()], 5000)
+}
+
+def off() {
+	log.info "off"
+	delayBetween ([zwave.basicV1.basicSet(value: 0x00).format(), zwave.switchMultilevelV1.switchMultilevelGet().format()], 5000)
+}
+
+def setLevel(value) {	   	
+    log.info "current level: ${device.currentValue("level")}"
+	delayBetween ([zwave.basicV1.basicSet(value: value).format(), zwave.switchMultilevelV1.switchMultilevelGet().format()], 5000)
+}
+
+def updateLevel(val){
+    log.info "updateLevel $val"   
+        
+    if (val == 0){ // I liked that 0 = off
+    	log.info "turning off"
+    	sendEvent(name:"level",value:val)
+        sendEvent(name:"switch.setLevel",value:val) // had to add this to work if apps subscribed to
+                                                    // setLevel event. "Dim With Me" was one.
+    	off()
+    }
+    else
+    {
+    	sendEvent(name:"level",value:val)
+        sendEvent(name:"switch.setLevel",value:val) // had to add this to work if apps subscribed to
+                                                    // setLevel event. "Dim With Me" was one.
+        setLevel(val)    	         
+    }
+}
+
+def levelUp(){
+    int nextLevel = device.currentValue("level") + 10
+    // make sure we don't drive switches past allowed values (command will hang device waiting for it to
+    // execute. Never commes back)
+    if( nextLevel > 100){
+    	nextLevel = 100
+    }
+    updateLevel(nextLevel)
+    log.info "level up $nextLevel"
+}
+
+def levelDown(){
+    int nextLevel = device.currentValue("level") - 10
+    // make sure we don't drive switches past allowed values (command will hang device waiting for it to
+    // execute. Never commes back)
+    if (nextLevel < 0){
+    	nextLevel = 0
+    }
+    updateLevel(nextLevel)
+    log.info "level down $nextLevel"
+}
+
+def setLevel(value, duration) {
+	def dimmingDuration = duration < 128 ? duration : 128 + Math.round(duration / 60)
+	zwave.switchMultilevelV2.switchMultilevelSet(value: value, dimmingDuration: dimmingDuration).format()
+}
+
+def poll() {
+	zwave.switchMultilevelV1.switchMultilevelGet().format()
+}
+
+def refresh() {
+	zwave.switchMultilevelV1.switchMultilevelGet().format()
+}
+
+def indicatorWhenOn() {
+	sendEvent(name: "indicatorStatus", value: "when on", display: false)
+	zwave.configurationV1.configurationSet(configurationValue: [1], parameterNumber: 3, size: 1).format()
+}
+
+def indicatorWhenOff() {
+	sendEvent(name: "indicatorStatus", value: "when off", display: false)
+	zwave.configurationV1.configurationSet(configurationValue: [0], parameterNumber: 3, size: 1).format()
+}
+
+def indicatorNever() {
+	sendEvent(name: "indicatorStatus", value: "never", display: false)
+	zwave.configurationV1.configurationSet(configurationValue: [2], parameterNumber: 3, size: 1).format()
+}
+
+def invertSwitch(invert=true) {
+	if (invert) {
+		zwave.configurationV1.configurationSet(configurationValue: [1], parameterNumber: 4, size: 1).format()
+	}
+	else {
+		zwave.configurationV1.configurationSet(configurationValue: [0], parameterNumber: 4, size: 1).format()
+	}
+}
